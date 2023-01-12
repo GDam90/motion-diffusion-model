1fbe45d51633508e14a6d9e0f25f0bdf633c8d74
L-Scofano <luca.scofano@uniroma1.it>
2023-01-12 16:37:08 +0100

    add comments

--- diffusion/gaussian_diffusion.py
+++ diffusion/gaussian_diffusion.py
@@ -5,6 +5,9 @@ https://github.com/hojonathanho/diffusion/blob/1e0dceb3b3495bbe19116a5e1b3596cd0
 
 Docstrings have been added, as well as DDIM sampling and a new collection of beta schedules.
 """
+import sys
+sys.path.append('/media/hdd/luca_s/code/DDPMotion/motion-diffusion-model')
+
 
 import enum
 import math
