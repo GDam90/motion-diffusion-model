daddb05cf4b7aa8020d197cf2911ca536a20e98e
GDam90 <guido.damely@gmail.com>
2023-01-26 18:30:31 +0100

    added methods to WandBPlatform

--- train/train_platforms.py
+++ train/train_platforms.py
@@ -75,6 +75,38 @@ class WandbPlatform(TrainPlatform):
     
     def get_run_id(self):
         return self.run.id
+    
+    def get_run_config(self):
+        '''
+        get_run_config method returns the config attribute of the 
+        wandb.run.Run object which is a dictionary of the command
+        line arguments and environment variables passed to the run.
+        '''
+        return self.run.config
+
+    def get_run_summary(self):
+        '''
+        get_run_summary method returns the summary attribute of 
+        the wandb.run.Run object which is a dictionary of the 
+        summary metrics that have been logged for the current run.
+        '''
+        return self.run.summary
+
+    def get_run_step(self):
+        '''
+        get_run_step method returns the step attribute of the 
+        wandb.run.Run object which is the global step of the 
+        training process (if the training is using steps).
+        '''
+        return self.run.step
+
+    def get_run_project(self):
+        '''
+        get_run_project method returns the project attribute 
+        of the wandb.run.Run object which is the name of the 
+        project that the run belongs to.
+        '''
+        return self.run.project
 
 class NoPlatform(TrainPlatform):
     def __init__(self, save_dir):
